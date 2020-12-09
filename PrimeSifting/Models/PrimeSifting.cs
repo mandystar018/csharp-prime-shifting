2321f42f23a984d69ccff68165018ca43689a16c
Shannon Grantski <grantski@pm.me>
2020-12-09 14:37:52 -0800

    take a detour into the how Loops work.
    Co-authored-by: Svea Wade <svealinneawade@gmail.com>
    Co-authored-by: Amanda Otero <mandystar018@gmail.com>

--- PrimeSifting/Models/PrimeSifting.cs
+++ PrimeSifting/Models/PrimeSifting.cs
@@ -1,9 +1,16 @@
+using System;
+
 namespace Sieve.Models
 {
   public class Prime
   {
-
+    static void Main()
+    {
+      int userNum = 5;
+      for (int i = 1 ; i <= userNum; i++)
+      {
+        Console.WriteLine(i);
+      }
+    }
   }
-
-
 }
